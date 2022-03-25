e3a236f645396b9e8d5d4aff6c6ad24e2afbbdb1
pounde candy <poundensigue@gmail.com>
2022-03-24 21:01:12 -0400

    creation de la classe node

---
+++ src/com/debut/MyLinkedList.java
@@ -0,0 +1,14 @@
+package com.debut;
+
+public class MyLinkedList {
+    public Node head;
+
+    public MyLinkedList() {
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
