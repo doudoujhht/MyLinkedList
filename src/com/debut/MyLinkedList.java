6bd1383f780167bcd0e931b5c804f4c7276ef837
pounde candy <poundensigue@gmail.com>
2022-03-24 23:12:40 -0400

    ajout des commentaire et de methodes qui prennent des String au lieu de node

--- src/com/debut/MyLinkedList.java
+++ src/com/debut/MyLinkedList.java
@@ -9,34 +9,66 @@ public class MyLinkedList {
     public MyLinkedList() {
         this.head=null;
     }
+
+    // un construteur dans lequel on passe des string pour en faire des Node
     public MyLinkedList(String... args) {
+        // chaque string creer un nouveau node et le rajoute à la fin
         for (String arg : args) {
-            Node node=new Node(arg);
-            this.addToTail(node);
+
+            Node node=new Node(arg);// ca creer un node avec le string
+            this.addToTail(node);//ca ajoute le nouveau node à la fin de la liste
         }
     }
 
     public MyLinkedList(Node... args) {
         for (Node arg : args) {
+            //ca ajoute les node à la fin de la liste
             this.addToTail(arg);
         }
     }
 
     public void addToHead(Node add){
-         if (head==null) head=add;
+         if (head==null) head=add;// si la liste est vide ajouter le node
+
          else {
+             //si la liste n'est pas vide on rajoute le nouveau node comme head et on met l'ancien head comme node suivant
              Node second=this.head;
              this.head=add;
              this.head.setNext(second);
          }
     }
+    public void addToHead(String node){
+        // le meme constructeur qu'en haut mais qui prends un String en parametre
+        Node add=new Node(node);
+        if (head==null) head=add;// si la liste est vide ajouter le node
+
+        else {
+            //si la liste n'est pas vide on rajoute le nouveau node comme head et on met l'ancien head comme node suivant
+            Node second=this.head;
+            this.head=add;
+            this.head.setNext(second);
+        }
+    }
 
     public void addToTail(Node add){
+        // si le la liste est vide rajouter l'element
         Node current=head;
         if (current==null) head=add;
         else {
             while (current.getNext()!=null) current=current.getNext();
             current.setNext(add);
+        }
+
+    }
+
+    public void addToTail(String node){
+        Node add=new Node(node);
+        // si le la liste est vide rajouter l'element
+        Node current=head;
+        if (current==null) head=add;        // si le la liste est vide rajouter l'element
+        else {
+            while (current.getNext()!=null) current=current.getNext();// faire le tour de la boucle pour retrouver le dernier node
+            current.setNext(add);//faire que le node apres le dernier soit le nouveau dernier
         }
 
     }
@@ -54,6 +86,7 @@ public class MyLinkedList {
     }
 
     public Node removeTail(){
+        // ca renvoit un node au qu'au ou on veut faire quelque chose avec le node suprimmé
         Node current=this.head;
         while (current.getNext().getNext()!=null){
             current=current.getNext();
@@ -63,6 +96,7 @@ public class MyLinkedList {
         return tail;
     }
 
+    // ca renvoit un node au qu'au ou on veut faire quelque chose avec le node suprimmé
     public Node removeHead(){
         Node current=head;
         head=head.getNext();
